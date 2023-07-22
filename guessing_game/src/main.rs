2fbb90f7eefaf6ff77a31b62eca967496d0a2cf6
Adam Children <adam.children@themix.org.uk>
2023-07-22 16:12:56 +0100

    chapter 2 - introduced counter

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,15 +2,21 @@ use rand::Rng;
 use rand::thread_rng;
 use std::cmp::Ordering;
 use std::io;
+use std::ops::RangeInclusive;
+
+//The kind of range expression we’re using here takes the form start..=end and is inclusive on the lower and upper bounds, so we need to specify 1..=100 to request a number between 1 and 100.
+const RANGE: RangeInclusive<u32> = 1..=50;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = thread_rng().gen_range(1..=100);
+    let secret_number = thread_rng().gen_range(RANGE);
+    let mut counter: u32 = 0;
 
     loop {
         println!("Please input your guess.");
 
+        // Mutable varriable (Can be)
         let mut guess = String::new();
 
         // Standard input from user with assertion attached for error handling
@@ -27,13 +33,14 @@ fn main() {
         // Result returns OK or Err based on conditional (Result is an inbuilt enum)
 
         println!("You guessed: {guess}");
-        
+        counter +=1;
+
         //comparing and ordering values. (CMP can be used as comparitor)
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! You took {counter} attempts");
                 break;
             }
         }
